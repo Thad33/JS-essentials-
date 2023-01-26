fa81e54d4caeab2dc452726fc6b058751381a44d
Thaddaeus <thaddaeusotieno@gmail.com>
2023-01-26 07:04:44 +0300

    h

---
+++ js.js
@@ -0,0 +1,17 @@
+//we have 20 string methods in JavaScript 
+
+//Sort, Slice, Push, Pop, 
+//Example:2
+let firstName1 = 'Asabeneh'
+let lastName1 = 'Yetayeh'
+let country1 = 'Finland'
+let city1 = 'Helsinki'
+let language1 = 'JavaScript'
+let job1 = 'teacher'
+let age = 250
+let fullName1 = firstName + ' ' + lastName
+
+let personInfoTwo = `I am ${fullName1}. I am ${age}. I live in ${country1}.` //ES6 - String interpolation method
+let personInfoThree = `I am ${fullName1}. I live in ${city1}, ${country1}. I am a ${job1}. I teach ${language1}.`
+console.log(personInfoTwo)
+console.log(personInfoThree)
