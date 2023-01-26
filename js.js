726ceef43e43f4207a5d0963d6a8ba4232b32013
Thaddaeus <thaddaeusotieno@gmail.com>
2023-01-26 08:03:28 +0300

    h

--- js.js
+++ js.js
@@ -9,9 +9,15 @@ let city1 = 'Helsinki'
 let language1 = 'JavaScript'
 let job1 = 'teacher'
 let age = 250
-let fullName1 = firstName + ' ' + lastName
+let fullName1 = firstName1 + ' ' + lastName1
 
 let personInfoTwo = `I am ${fullName1}. I am ${age}. I live in ${country1}.` //ES6 - String interpolation method
 let personInfoThree = `I am ${fullName1}. I live in ${city1}, ${country1}. I am a ${job1}. I teach ${language1}.`
 console.log(personInfoTwo)
 console.log(personInfoThree)
+//
+let zoa = "Green"
+console.log(zona.length) //10
+
+let zo = "Green"
+console.log(zo.indexOf[1])
